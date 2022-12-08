f2fa5168a162a88a8b3cdb04c7123bc67e82f5a2
Ivan Martinez-Ortiz <imartinezortiz@gmail.com>
2022-12-08 19:24:57 +0100

    Añadiendo métodos que parecían eliminados de la interfaz GameWorld

--- src/tp1/p2/logic/GameWorld.java
+++ src/tp1/p2/logic/GameWorld.java
@@ -2,12 +2,21 @@ package tp1.p2.logic;
 
 import tp1.p2.control.Level;
 import tp1.p2.control.exceptions.GameException;
+import tp1.p2.logic.gameobjects.GameObject;
 
 public interface GameWorld {
 
 	public static final int NUM_ROWS = 4;
 
 	public static final int NUM_COLS = 8;
+
+	// TODO add your code here
+
+	void addSun();
+
+	void tryToCatchObject(int col, int row) throws GameException;
+
+	boolean addItem(GameObject gameObject);
 
 	void update() throws GameException;
 
@@ -21,8 +30,6 @@ public interface GameWorld {
 
 	void checkValidZombiePosition(int col, int row) throws GameException;
 
-	void tryToCatchObject(int col, int row) throws GameException;
 
-	// TODO add your code here
 
 }
